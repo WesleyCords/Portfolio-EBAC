270a98bf76d6a718e546aa7545ff6ed458ff2ec3
Wesley <Wesley@teste.com>
2025-07-06 17:52:41 -0300

    Construindo a estruturação de arquivos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img
+      src="https://github.com/wesleycords.png"
+      alt="Avatar do Perfil do Github"
+    />
+    <Titulo> Wesley Cordeiro</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo> Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, nam nulla
+      distinctio nobis modi maiores laborum asperiores, iusto vitae in fugit ut
+      adipisci numquam aut non accusamus autem, est voluptas.
+    </p>
+  </section>
+)
+
+export default Sobre
